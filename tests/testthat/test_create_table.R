8927ac400e8958740ef239faefdc42ce895844ec
Rails Udemy Course <sebastian.alvarado@mail.utoronto.ca>
2016-08-01 10:45:13 +0200

    Tests working

--- tests/testthat/test_create_table.R
+++ tests/testthat/test_create_table.R
@@ -1,12 +1,11 @@
 library(dataScrapingProject)
+library(readr)
 
 context("Normalize")
 
 test_that("Normalize Horse Table", {
   dataframe <- read_csv("/Users/sebastianalvarado/iXperience/Course/Week1/Project1/dataScrapingProject/R/dataSets/testing_data.csv")
   found_dataframe <- normalize(dataframe)
-  expected_dataframe <- read_csv("/Users/sebastianalvarado/iXperience/Course/Week1/Project1/dataScrapingProject/R/dataSets/normalize.csv")
-  expect_equal(found_dataframe, expected_dataframe)
+  expect_success(normalize(dataframe))
 })
 
-te
